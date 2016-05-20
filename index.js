ee7610d14e2ff5d9048f827845667df743b940a9
guilhermeaandrade <guilhermealvarengaandrade@gmail.com>
2016-05-19 21:57:30 -0300

    Alterando apresentação  da pagina inicial

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
