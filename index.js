b9496be49f7239cf09aed28d77292fd1c106b685
guilhermeaandrade <guilhermealvarengaandrade@gmail.com>
2016-05-21 14:47:02 -0300

    First Commit: esquematizando app, instalando modulos Node, acrescentando pasta de testes

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var body_parser = require('body-parser');
+var http = require('http');
+var path = require('path');
+var aws = require('aws-sdk');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -8,10 +13,45 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(body_parser.urlencoded({ extended: true }));
+
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/sign_s3', function(req, res){
+    aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+    var s3 = new aws.S3();
+    var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: req.query.file_name,
+        Expires: 120,
+        ContentType: req.query.file_type,
+        ACL: 'public-read'
+    };
+    s3.getSignedUrl('putObject', s3_params, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var return_data = {
+                signed_request: data,
+                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+            };
+            res.write(JSON.stringify(return_data));
+            res.end();
+        }
+    });
+});
+
+app.post('/success', function(request, response){
+    response.render('pages/success');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
