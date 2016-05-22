5a7229cd73149a0464699a6e7de7bb202a99daeb
guilhermeaandrade <guilhermealvarengaandrade@gmail.com>
2016-05-22 15:11:07 -0300

    Acrescentando alert e implementando codigo para informar andamento do processo no Zencoder

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
 var S3_BUCKET = process.env.S3_BUCKET;
 
 var aws = require('aws-sdk');
-    //s3Stream = require('s3-upload-stream');
 
 var app = express();
 
@@ -20,20 +19,44 @@ var app = express();
 var Zencoder = require('zencoder');
 var client = new Zencoder('aecec04ea8904ffcadee0cb7a1597790');
 client.Job.create({
-  input: 'https://s3-sa-east-1.amazonaws.com/apsambatech/test0.wmv',
+  input: 'https://s3-sa-east-1.amazonaws.com/apsambatech/test1.wmv',
   outputs: [{
-      url: "s3://apsambatech/test0_converted.mp4",
+      url: "s3://apsambatech/test1_converted.mp4",
       size: "640x480"
     },
     {
-      url: "s3://apsambatech/test0_converted.webm",
+      url: "s3://apsambatech/test1_converted.webm",
       size: "640x480"
     }
   ]
 }, function(err, data){
-  if (err) { console.log("OH NO! There was an error"); return err; }
+  if (err) { console.log("Erro com a conversão!"); return err; }
   console.log('Job created!\nJob ID: ' + data.id);
+  poll(data.id);
 });
+
+function poll(id) {
+  setTimeout(function(){
+    client.Job.progress(id, function(err, data) {
+      if (err) { console.log("OH NO! There was an error"); return err; }
+      if (data.state == 'waiting') {
+        if (!this.status || this.status != 'waiting') {
+            this.status = 'waiting'; // set status to waiting so we can start adding dots.
+            console.log("Waiting");
+        }
+        poll(id);
+      } else if (data.state == 'processing') {
+          var progress = Math.round(data.progress * 100) / 100; // round to nearest decimal places.
+          this.status = 'processing'; // not important, but makes sure we don't display waiting again
+          console.log('Processando');
+          poll(id);
+      } else if (data.state == 'finished') {
+        console.log('Job finished!'); // finished!
+        process.exit(0); // exit
+      }
+    });
+  }, 5000);
+}
 
 //Configurações da aplicação
 app.set('port', (process.env.PORT || 5000));
@@ -42,11 +65,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(body_parser.urlencoded({ extended: true }));
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
-app.post('/success', function(request, response){
-    response.render('pages/success');
+app.post('/', function(request, response){
+    response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
