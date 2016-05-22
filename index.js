e888b58c75431e7903578def79a8c58e20e0c8b6
guilhermeaandrade <guilhermealvarengaandrade@gmail.com>
2016-05-21 22:38:08 -0300

    Inserção de código teste para verificar funcionamento da api do Zencoder

--- index.js
+++ index.js
@@ -2,21 +2,55 @@ var express = require('express');
 var body_parser = require('body-parser');
 var http = require('http');
 var path = require('path');
+var fs = require('fs');
+var zlib = require('zlib');
+
+//upload S3
+//definindo variáveis de acesso
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
+
 var aws = require('aws-sdk');
+    //s3Stream = require('s3-upload-stream');
 
 var app = express();
 
+//zencoder
+var Zencoder = require('zencoder');
+var client = new Zencoder('aecec04ea8904ffcadee0cb7a1597790');
+client.Job.create({
+  input: 'https://s3-sa-east-1.amazonaws.com/apsambatech/test0.wmv',
+  outputs: [{
+      url: "s3://apsambatech/test0_converted.mp4",
+      size: "640x480"
+    },
+    {
+      url: "s3://apsambatech/test0_converted.webm",
+      size: "640x480"
+    }
+  ]
+}, function(err, data){
+  if (err) { console.log("OH NO! There was an error"); return err; }
+  console.log('Job created!\nJob ID: ' + data.id);
+});
+
+//Configurações da aplicação
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
 app.use(body_parser.urlencoded({ extended: true }));
-
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.post('/success', function(request, response){
+    response.render('pages/success');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 app.get('/sign_s3', function(req, res){
@@ -43,15 +77,3 @@ app.get('/sign_s3', function(req, res){
         }
     });
 });
-
-app.post('/success', function(request, response){
-    response.render('pages/success');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-var S3_BUCKET = process.env.S3_BUCKET;
