5a6bbc7fc45aa9f87588d3ce090ae807083f301b
guilhermeaandrade <guilhermealvarengaandrade@gmail.com>
2016-05-24 13:32:35 -0300

    Estruturando código para integrar o zencoder e remoção de modulos não utilizados

--- index.js
+++ index.js
@@ -2,62 +2,11 @@ var express = require('express');
 var body_parser = require('body-parser');
 var http = require('http');
 var path = require('path');
+var aws = require('aws-sdk');
+var Zencoder = require('zencoder');
 
 var app = express();
 
-//upload S3
-//*****************************************************************************************************
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-var S3_BUCKET = process.env.S3_BUCKET;
-
-var aws = require('aws-sdk');
-//****************************************************************************************************
-
-//zencoder
-//*****************************************************************************************************
-var Zencoder = require('zencoder');
-var client = new Zencoder('aecec04ea8904ffcadee0cb7a1597790');
-client.Job.create({
-  input: 'https://s3-sa-east-1.amazonaws.com/apsambatech/test8.wmv',
-  outputs: [{
-      url: "s3://apsambatech/test8.mp4",
-      size: "640x480"
-    },
-    {
-      url: "s3://apsambatech/test8.webm",
-      size: "640x480"
-    }
-  ],
-  test: false,
-}, function(err, data){
-  if (err) { console.log("Erro com a conversão!"); return err; }
-  console.log('Job created!\nJob ID: ' + data.id);
-  poll(data.id);
-});
-
-function poll(id) {
-  setTimeout(function(){
-    client.Job.progress(id, function(err, data) {
-      if (err) { console.log("OH NO! There was an error"); return err; }
-      if (data.state == 'waiting') {
-        if (!this.status || this.status != 'waiting') { //processo esperando
-            this.status = 'waiting';
-            console.log("Waiting");
-        }
-        poll(id);
-      } else if (data.state == 'processing') { //processo em andamento
-          var progress = Math.round(data.progress * 100) / 100;
-          this.status = 'processing';
-          console.log('Processando '+ progress + "%");
-          poll(id);
-      } else if (data.state == 'finished') { //processo finalizado
-        console.log('Job finished!');
-      }
-    });
-  }, 5000);
-}
-//************************************************************************************************************
 //Configurações da aplicação
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -71,6 +20,12 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+//upload S3
+//*****************************************************************************************************
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
 app.get('/sign_s3', function(req, res){
     aws.config.update({
@@ -102,3 +57,54 @@ app.get('/sign_s3', function(req, res){
         }
     });
 });
+
+//zencoder
+//*****************************************************************************************************
+app.get('/zencoder', function(req, res){
+    var client = new Zencoder('aecec04ea8904ffcadee0cb7a1597790');
+    console.log("url: "+req.query.url);
+    console.log("file_name: " + req.query.file_name);
+    client.Job.create({
+        input: req.query.url,
+        outputs: [{
+            url: 's3://'+S3_BUCKET+'/'+req.query.file_name,
+            size: "640x480",
+        }],
+        test: false,
+    }, function(err, data){
+        var response = 0;
+        if(err){
+            console.log("Erro com a conversão!");
+            return err;
+        }else{
+            response = poll(data.id, client);
+        }
+        console.log('Job created!\nJob ID: ' + data.id);
+        res.write(JSON.stringify(""+response));
+        res.end();
+    });
+});
+
+function poll(id, client) {
+  console.log("entrei no metodo poll");
+  setTimeout(function(){
+    client.Job.progress(id, function(err, data) {
+      if (err) { console.log("OH NO! There was an error"); return err; }
+      if (data.state == 'waiting') {
+        if (!this.status || this.status != 'waiting') { //processo esperando
+            this.status = 'waiting';
+            console.log("Waiting");
+        }
+        poll(id, client);
+      } else if (data.state == 'processing') { //processo em andamento
+          var progress = Math.round(data.progress * 100) / 100;
+          this.status = 'processing';
+          console.log('Processando '+ progress + "%");
+          poll(id, client);
+      } else if (data.state == 'finished') { //processo finalizado
+        console.log('Job finished!');
+        return 1;
+      }
+    });
+  }, 5000);
+}
